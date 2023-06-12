aea2abb3a1a76756356e525db6de609ee8b640ab
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-06-12 15:45:33 -0400

    Functions Parameter Lab
    Javascript Lab

---
+++ index.jsparameter.js
@@ -0,0 +1,12 @@
+function logTwoValues(value1, value2)  {
+    console.log('The two values are ${value1} and $ {value2}.  ') :
+
+}
+function sayHelloTo(firstName) = "User)" {
+    console.log('Hello, $ {firstName} ! ');
+}
+
+
+IntroductionWithLanguageOptional (first name) = "Language)" {
+    console.llog('Hello, $ (firstname) ! ');
+}
